80ebe8e93c6e734f2eaacfeee1adbb6c353b8fa3
Alexis <isalexis.design@gmail.com>
2026-03-06 18:47:15 +0100

    Implementado sistema de autenticación con Laravel Sanctum

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\AuthController;
 
 
 Route::get('/ping', function () {
@@ -13,4 +10,21 @@ Route::get('/ping', function () {
         'status' => 'ok',
         'message' => '¡Hola desde el Backend de Glucode! (Ahora, con auto-deploy funcionando)'
     ]);
+});
+
+
+// Rutas públicas
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+// Rutas privadas
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
 });
