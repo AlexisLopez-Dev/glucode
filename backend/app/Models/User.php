3a0c7eeb6a50f6103a56e2c75a5773a6c6ec56ee
Alexis <isalexis.design@gmail.com>
2026-03-06 18:47:15 +0100

    Implementado sistema de autenticación con Laravel Sanctum

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -8,8 +8,10 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable {
+
     public function medicalSetting(): HasOne {
         return $this->hasOne(MedicalSetting::class);
     }
@@ -19,7 +21,7 @@ class User extends Authenticatable {
     }
 
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -54,4 +56,5 @@ class User extends Authenticatable {
             'password' => 'hashed',
         ];
     }
+
 }
