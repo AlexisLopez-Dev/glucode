033fc0ed0d520496050e0f43e7d9869e860f411b
Alexis <isalexis.design@gmail.com>
2026-03-06 12:35:47 +0100

    Creada la migración para la tabla medical_settings, y relación 1:1 indicada en los modelos User:MedicalSetting

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
+    public function medicalSetting(): HasOne {
+        return $this->hasOne(MedicalSetting::class);
+    }
+
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
