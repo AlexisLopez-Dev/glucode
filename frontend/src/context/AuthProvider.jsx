80eba353949282a2c1657050ff06313ed7b0d48b
Alexis <isalexis.design@gmail.com>
2026-03-07 17:41:54 +0100

    Añadida la gestión de autenticación con AuthProvider y AuthContext. Se implementaron rutas protegidas en AppRouter y se configuró la verificación de token en el inicio de sesión.

---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import axios from '../lib/axios';
+import { AuthContext } from './AuthContext';
+
+export const AuthProvider = ({ children }) => {
+  
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = localStorage.getItem('auth_token');
+      
+      if (token) {
+        try {
+          const response = await axios.get('/user');
+          setUser(response.data);
+          setIsAuthenticated(true);
+        } catch (error) {
+          console.error("Token inválido o expirado: " + error);
+          localStorage.removeItem('auth_token');
+          setIsAuthenticated(false);
+          setUser(null);
+        }
+      }
+      setIsLoading(false);
+    };
+
+    checkAuth();
+  }, []);
+
+  const login = (token) => {
+    localStorage.setItem('auth_token', token);
+    setIsAuthenticated(true);
+  };
+
+  const logout = async () => {
+    try {
+      await axios.post('/logout');
+    } catch (error) {
+      console.error("Error al hacer logout", error);
+    } finally {
+      localStorage.removeItem('auth_token');
+      setUser(null);
+      setIsAuthenticated(false);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, isLoading, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+  
+}
