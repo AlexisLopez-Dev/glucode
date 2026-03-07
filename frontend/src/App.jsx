634d9900722a005740b039738943c872402b0f4c
Alexis <isalexis.design@gmail.com>
2026-03-07 17:14:21 +0100

    Añadida la funcionalidad de inicio de sesión y la página de Dashboard. Se incorporó react-hook-form para la gestión de formularios y se configuraron las primeras rutas de navegación.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,9 @@
-import { useState } from 'react'
-import { API_BASE_URL } from './config';
+import { AppRouter } from './routers/AppRouter';
 
 function App() {
-  const [mensaje, setMensaje] = useState('Esperando conexión con el backend...')
-
-  const hacerPing = async () => {
-    try {
-      const response = await fetch(`${API_BASE_URL}/ping`);
-      const data = await response.json()
-      setMensaje(data.message)
-    } catch (error) {
-      setMensaje('Error de conexión con el servidor: ' + error)
-    }
-  }
-
   return (
-    <div>
-      <h1>Glucode: Walking Skeleton</h1>
-      <button onClick={hacerPing} >Conectar con Laravel</button>
-      <p>{mensaje}</p>
-    </div>
-  )
+    <AppRouter />
+  );
 }
 
-export default App
+export default App;
