0630e23a2dbc058fb0fbf9ad2884b5959f833f2e
Alexis <isalexis.design@gmail.com>
2026-03-07 18:51:17 +0100

    Mejorada la validación de la contraseña en el registro de usuarios: se añadió la confirmación de contraseña al proceso de validación.

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller {
         $validado = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:6|confirmed',
         ]);
 
         $user = User::create($validado);
